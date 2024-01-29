7f01711dc02dd714623a9d566b015744df3f76f3
Dwaipayan Som <78339312+DwaipayanSom@users.noreply.github.com>
2024-01-29 20:22:25 +0530

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,6 +7,7 @@ pipeline {
         SERVER_IMAGE_NAME = 'examor/server'
         DATABASE_IMAGE_NAME = 'examor/database'
         IMAGE_TAG = 'latest'
+        NETWORK_NAME = 'examor-network'
     }
 
     stages {
@@ -26,6 +27,50 @@ pipeline {
             }
         }
 
+        stage('Create Docker Network') {
+            steps {
+                script {
+                    def existingNetwork = sh(script: "docker network ls -q -f name=${NETWORK_NAME}", returnStatus: true).toString().trim()
+                    
+                    if (existingNetwork.isEmpty()) {
+                        sh "docker network create ${NETWORK_NAME}"
+                    } else {
+                        echo "Network ${NETWORK_NAME} already exists."
+                    }
+                }
+            }
+        }
+
+        stage('Run Containers in the Network') {
+        steps {
+            script {
+                def databaseContainerExists = sh(script: "docker ps -q -f name=database", returnStatus: true).toString().trim()
+                def serverContainerExists = sh(script: "docker ps -q -f name=server", returnStatus: true).toString().trim()
+                def appContainerExists = sh(script: "docker ps -q -f name=app", returnStatus: true).toString().trim()
+
+                if (databaseContainerExists) {
+                sh "docker stop database"
+                sh "docker rm database"
+                }
+
+                if (serverContainerExists) {
+                sh "docker stop server"
+                sh "docker rm server"
+                }
+
+                if (appContainerExists) {
+                sh "docker stop app"
+                sh "docker rm app"
+                }
+
+                sh "docker run -d --network ${NETWORK_NAME} -p 52020:3306 --name database ${DOCKER_REGISTRY}/${DATABASE_IMAGE_NAME}:${IMAGE_TAG}"
+                sh "docker run -d --network ${NETWORK_NAME} -p 51717:51717 --name server ${DOCKER_REGISTRY}/${SERVER_IMAGE_NAME}:${IMAGE_TAG}"
+                sh "docker run -d --network ${NETWORK_NAME} -p 51818:51818 --name app ${DOCKER_REGISTRY}/${APP_IMAGE_NAME}:${IMAGE_TAG} /bin/sh -c 'sleep 15 && pnpm dev:docker'"
+            }
+        }
+    }
+
+        
         stage('Deploy to Minikube') {
             steps {
                 script {
@@ -43,6 +88,5 @@ pipeline {
                 }
             }
         }
-
     }
 }
